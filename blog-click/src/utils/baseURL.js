919061b43a4d4d30eb6898f1a407578f9657e9a6
py <1580559335@qq.com>
2020-09-30 17:05:11 +0800

    目前后端只能连接本地接口

--- blog-click/src/utils/baseURL.js
+++ blog-click/src/utils/baseURL.js
@@ -1,5 +1,5 @@
-export const baseurl = 'https://www.ximengpy.top/' // 基本路径
-// export const baseurl = 'http://localhost/' // 基本路径
+// export const baseurl = 'https://www.ximengpy.top/' // 基本路径
+export const baseurl = 'http://localhost/' // 基本路径
 
 export const Avatar = baseurl+'api/upload/avatar'  //头像上传地址
 // server{
